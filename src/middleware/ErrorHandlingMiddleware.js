b2409c4fb9a8788e0fd2421709760d913ea80298
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-17 13:03:55 +0300

    complete readme.md

--- src/middleware/ErrorHandlingMiddleware.js
+++ src/middleware/ErrorHandlingMiddleware.js
@@ -5,5 +5,5 @@ module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
-  return res.status(00).json({ message: "Не предвиденная ошибка" });
+  return res.status(400).json({ message: "Не предвиденная ошибка" });
 };
